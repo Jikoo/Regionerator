e7c3606fb1a5991e1073eda744403acd466c6412
Jikoo <Jikoo.Games@gmail.com>
2015-06-28 07:03:32 -0400

    Delete individual chunks More like abandon and hope Minecraft cleans up, but whatever. Allow regeneration for.

--- src/com/github/jikoo/regionerator/DeletionRunnable.java
+++ src/com/github/jikoo/regionerator/DeletionRunnable.java
@@ -2,6 +2,8 @@ package com.github.jikoo.regionerator;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -18,6 +20,8 @@ import org.bukkit.scheduler.BukkitRunnable;
  */
 public class DeletionRunnable extends BukkitRunnable {
 
+	private static final String STATS_FORMAT = "%s - Checked %s/%s, deleted %s regions and %s chunks";
+
 	private final Regionerator plugin;
 	private final World world;
 	private final File regionFileFolder;
@@ -29,7 +33,7 @@ public class DeletionRunnable extends BukkitRunnable {
 	private int dZ = 0;
 	private final ArrayList<Pair<Integer, Integer>> regionChunks = new ArrayList<>();
 	private int regionsDeleted = 0;
-	private final int chunksDeleted = 0;
+	private int chunksDeleted = 0;
 	private long nextRun = Long.MAX_VALUE;
 
 	public DeletionRunnable(Regionerator plugin, World world) {
@@ -86,7 +90,9 @@ public class DeletionRunnable extends BukkitRunnable {
 		int chunkX = regionChunkX + dX;
 		int chunkZ = regionChunkZ + dZ;
 		VisitStatus status = plugin.getFlagger().getChunkVisitStatus(world, chunkX, chunkZ);
-		if (status.ordinal() <= VisitStatus.UNVISITED.ordinal()) {
+		if (status.ordinal() <= VisitStatus.UNVISITED.ordinal()
+				|| plugin.getGenerateFlag() == Long.MAX_VALUE
+				&& status.ordinal() <= VisitStatus.UNVISITED.ordinal()) {
 			// Add generated flagged chunks
 			// Deleting an entire region is much faster (and more space-efficient) than deleting chunks
 			regionChunks.add(new ImmutablePair<Integer, Integer>(chunkX, chunkZ));
@@ -114,12 +120,57 @@ public class DeletionRunnable extends BukkitRunnable {
 				plugin.getFlagger().unflagRegion(world.getName(), regionChunkX, regionChunkZ);
 			}
 		} else if (regionChunks.size() > 0) {
-			// Delete individual chunks
-			// TODO implement
+			/*
+			 * For dealing with manually wiping individual chunks, heavily rewrite and adapt
+			 * @Brettflan's code from WorldBorder - this is relatively complex, and I'd like to
+			 * start off with a working solution.
+			 */
+			String regionFileName = regions[count];
+			File regionFile = new File(regionFileFolder, regionFileName);
+
+			if (regionFile.canWrite() || regionFile.setWritable(true) && regionFile.canWrite()) {
+				try (RandomAccessFile regionRandomAccess = new RandomAccessFile(regionFile, "rwd")) {
+					int chunkCount = 0;
+					for (Pair<Integer, Integer> chunkCoords : regionChunks) {
+						// Pointers for chunks are 4 byte integers stored at coordinates relative to the region file itself.
+						long chunkPointer = 4 * (chunkCoords.getLeft() - regionChunkX + (chunkCoords.getRight() - regionChunkZ) * 32);
+
+						regionRandomAccess.seek(chunkPointer);
+
+						/*
+						 * Here, WorldBorder opens up a new RandomAccessFile and reads all of the
+						 * data for each chunk, then caches it. That seems like a lot of useless
+						 * overhead, so rather than directly copy, we just read the individual
+						 * chunk's pointer.
+						 */
+						if (regionRandomAccess.readInt() == 0) {
+							continue;
+						}
+
+						// Seek back to the right position - reading the int moved us forward 4 bytes
+						regionRandomAccess.seek(chunkPointer);
+
+						/*
+						 * Note from @Brettflan: This method isn't perfect since the actual chunk
+						 * data is left orphaned, but Minecraft will overwrite the orphaned data
+						 * sector if/when another chunk is created in the region, so it's not so bad
+						 */
+						regionRandomAccess.writeInt(0);
+						chunkCount++;
+					}
+
+					regionRandomAccess.close();
+					chunksDeleted += chunkCount;
+
+				} catch (IOException ex) {}
+			}
 		}
+
+		// Reset and increment
+		regionChunks.clear();
 		count++;
 		if (plugin.debug(DebugLevel.LOW) && count % 20 == 0 && count > 0) {
-			plugin.debug(world.getName() + " - Checked " + count + ", deleted " + regionsDeleted + " regions and " + chunksDeleted + " chunks");
+			plugin.debug(getRunStats());
 			// TODO count/total
 		}
 		if (count < regions.length) {
@@ -131,7 +182,7 @@ public class DeletionRunnable extends BukkitRunnable {
 			regionChunkZ = regionChunkCoordinates.getRight();
 
 			if (plugin.debug(DebugLevel.HIGH)) {
-				plugin.debug("Checking region file " + count + ": " + regions[count]);
+				plugin.debug("Checking region " + count + " of " + regions.length + ": " + regions[count]);
 				// TODO count/total
 			}
 		} else {
@@ -140,7 +191,7 @@ public class DeletionRunnable extends BukkitRunnable {
 	}
 
 	public String getRunStats() {
-		return new StringBuilder(world.getName()).append(" - Checked ").append(count).append(", deleted ").append(regionsDeleted).toString();
+		return String.format(STATS_FORMAT, world.getName(), count, regions.length, regionsDeleted, chunksDeleted);
 	}
 
 	public long getNextRun() {
