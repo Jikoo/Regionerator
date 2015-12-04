ee3d2118e014c684e042dd5b45f613fc977d3d56
Jikoo <Jikoo.Games@gmail.com>
2015-12-04 15:19:33 -0500

    Using new GP claims API - if you are using GP, it must be 12.9.1 or higher

--- src/main/java/com/github/jikoo/regionerator/hooks/GriefPreventionHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/GriefPreventionHook.java
@@ -4,7 +4,7 @@ import org.bukkit.World;
 
 import com.github.jikoo.regionerator.Hook;
 
-import me.ryanhamshire.GriefPrevention.CreateClaimResult;
+import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 
 /**
@@ -20,14 +20,11 @@ public class GriefPreventionHook extends Hook {
 
 	@Override
 	public boolean isChunkProtected(World world, int chunkX, int chunkZ) {
-		// TODO http://dev.bukkit.org/bukkit-plugins/grief-prevention/tickets/910 change has been made, next release
-		// Creating an admin claim over the entire chunk is nearly as effective as just checking the chunk claims.
-		int x = chunkX << 4;
-		int z = chunkZ << 4;
-		CreateClaimResult result = GriefPrevention.instance.dataStore.createClaim(world, x, x + 15, 0, 255, z, z + 15, null, null, null, null);
-		if (result.succeeded) {
-			GriefPrevention.instance.dataStore.deleteClaim(result.claim);
+		for (Claim claim : GriefPrevention.instance.dataStore.getClaims(chunkX, chunkZ)) {
+			if (claim.getGreaterBoundaryCorner().getWorld().equals(world)) {
+				return true;
+			}
 		}
-		return !result.succeeded;
+		return false;
 	}
 }
