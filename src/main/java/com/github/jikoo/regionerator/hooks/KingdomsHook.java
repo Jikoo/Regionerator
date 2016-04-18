65de0f2c60aa329bafe03c80e30bf8a6e2ae5844
Jikoo <Jikoo.Games@gmail.com>
2016-04-18 15:02:00 -0400

    Added Kingdoms hook, bumped version to 1.3.3 for release

---
+++ src/main/java/com/github/jikoo/regionerator/hooks/KingdomsHook.java
@@ -0,0 +1,29 @@
+package com.github.jikoo.regionerator.hooks;
+
+import org.bukkit.World;
+
+import org.kingdoms.constants.land.Land;
+import org.kingdoms.constants.land.SimpleChunkLocation;
+import org.kingdoms.manager.game.GameManagement;
+
+import com.github.jikoo.regionerator.Hook;
+
+/**
+ * Hook for the plugin <a href=https://www.spigotmc.org/resources/kingdoms-battle-for-land-might-and-glory-1-8-x-1-9.11833/>Kingdoms</a>.
+ * 
+ * @author Jikoo
+ */
+public class KingdomsHook extends Hook {
+
+	public KingdomsHook() {
+		super("Kingdoms");
+	}
+
+	@Override
+	public boolean isChunkProtected(World chunkWorld, int chunkX, int chunkZ) {
+		SimpleChunkLocation chunkLocation = new SimpleChunkLocation(chunkWorld.getName(), chunkX, chunkZ);
+		Land land = GameManagement.getLandManager().getOrLoadLand(chunkLocation);
+		return land.getOwner() != null;
+	}
+
+}
