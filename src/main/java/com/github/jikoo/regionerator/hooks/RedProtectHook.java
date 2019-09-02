3fe219cf5d93cf9b70080e35e4d0dad9131a39fb
Jikoo <jikoo.games@gmail.com>
2019-09-02 13:00:54 -0400

    Update RedProtect hook for RedProtect 7.6.X

--- src/main/java/com/github/jikoo/regionerator/hooks/RedProtectHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/RedProtectHook.java
@@ -6,6 +6,8 @@ import br.net.fabiozumbi12.RedProtect.Bukkit.Region;
 import com.github.jikoo.regionerator.CoordinateConversions;
 import com.github.jikoo.regionerator.PluginHook;
 
+import com.github.jikoo.regionerator.world.DummyChunk;
+import org.bukkit.Location;
 import org.bukkit.World;
 
 /**
@@ -15,29 +17,49 @@ import org.bukkit.World;
  */
 public class RedProtectHook extends PluginHook {
 
+	private boolean needAPI = false;
+
 	public RedProtectHook() {
 		super("RedProtect");
 	}
 
 	@Override
+	public boolean isHookUsable() {
+		try {
+			return super.isHookUsable();
+		} catch (Exception | NoSuchMethodError | NoSuchFieldError e) {
+			System.err.println("RedProtect fast hook failed usability! Falling back to slower API-based solution.");
+			System.err.println("Please report this stack trace:");
+			e.printStackTrace();
+			needAPI = true;
+		}
+
+		return super.isHookUsable();
+	}
+
+	@Override
 	public boolean isChunkProtected(World chunkWorld, int chunkX, int chunkZ) {
 		/*
-		 * RedProtectAPI is somewhat lacking. Also, it contains a typo. While I'd bet it's pretty
-		 * efficient and good, the code style is utterly horrifying.
-		 *
-		 * API-only solution:
-		 * return RedProtectAPI.getChunkRegions(new DummyChunk(chunkWorld, chunkX, chunkZ)).size() > 0;
-		 * However, that method is new in 6.6.0, and I can perform the needed checks faster avoiding it.
+		 * RedProtectAPI is somewhat lacking and contains several typos.
+		 * Check is far more efficient to perform off the API.
 		 */
-		for (Region region : RedProtect.get().rm.getRegionsByWorld(chunkWorld)) {
-			if (CoordinateConversions.blockToChunk(region.getMinMbrX()) > chunkX
-					|| CoordinateConversions.blockToChunk(region.getMaxMbrX()) < chunkX
-					|| CoordinateConversions.blockToChunk(region.getMinMbrZ()) > chunkZ
-					|| CoordinateConversions.blockToChunk(region.getMaxMbrZ()) < chunkZ) {
+
+		if (needAPI) {
+			return RedProtect.get().getAPI().getChunkRegions(new DummyChunk(chunkWorld, chunkX, chunkZ)).size() > 0;
+		}
+
+		for (Region region : RedProtect.get().rm.getRegionsByWorld(chunkWorld.getName())) {
+			Location min = region.getMinLocation();
+			Location max = region.getMaxLocation();
+			if (CoordinateConversions.blockToChunk(min.getBlockX()) > chunkX
+					|| CoordinateConversions.blockToChunk(max.getBlockX()) < chunkX
+					|| CoordinateConversions.blockToChunk(min.getBlockZ()) > chunkZ
+					|| CoordinateConversions.blockToChunk(max.getBlockZ()) < chunkZ) {
 				continue;
 			}
 			return true;
 		}
+
 		return false;
 	}
 
