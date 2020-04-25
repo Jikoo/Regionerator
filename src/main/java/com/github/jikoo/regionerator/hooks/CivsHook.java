eb208c274b773180a0a03b9f37bb506bd8c7707a
Jikoo <jikoo.games@gmail.com>
2020-04-25 18:16:15 -0400

    Update Civs to 1.6.7

--- src/main/java/com/github/jikoo/regionerator/hooks/CivsHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/CivsHook.java
@@ -4,7 +4,13 @@ import com.github.jikoo.regionerator.Coords;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.redcastlemedia.multitallented.civs.regions.RegionManager;
+import org.redcastlemedia.multitallented.civs.regions.RegionPoints;
 
+/**
+ * PluginHook for <a href=https://www.spigotmc.org/resources/civs.67350/>Civs</a>.
+ *
+ * @author Jikoo
+ */
 public class CivsHook extends PluginHook {
 
 	public CivsHook() {
@@ -15,7 +21,7 @@ public class CivsHook extends PluginHook {
 	public boolean isChunkProtected(World chunkWorld, int chunkX, int chunkZ) {
 		return RegionManager.getInstance().getRegionsXYZ(
 				new Location(chunkWorld, Coords.chunkToBlock(chunkX), 0, Coords.chunkToBlock(chunkZ)),
-				16, 0, 255, 0, 16, 0, false).size() > 0;
+				new RegionPoints(16, 0, 255, 0, 16, 0), false).size() > 0;
 	}
 
 }
