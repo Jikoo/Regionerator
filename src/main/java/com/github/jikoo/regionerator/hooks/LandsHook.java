d2e420bb6732ce12130eaf9880293fb79a7e3c33
Angeschossen <!FlashLight2018>
2020-08-21 10:02:26 +0200

    Initialize LandsIntegration class

--- src/main/java/com/github/jikoo/regionerator/hooks/LandsHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/LandsHook.java
@@ -3,6 +3,7 @@ package com.github.jikoo.regionerator.hooks;
 import com.github.jikoo.regionerator.Regionerator;
 import me.angeschossen.lands.api.integration.LandsIntegration;
 import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * PluginHook for <a href=https://www.spigotmc.org/resources/lands.53313/>Lands</a>.
@@ -32,6 +33,7 @@ public class LandsHook extends PluginHook {
         return true;
     }
 
+    @NotNull
     private LandsIntegration getLandsAPI() {
         if (landsAPI == null) {
             landsAPI = new LandsIntegration(Regionerator.getPlugin(Regionerator.class));
