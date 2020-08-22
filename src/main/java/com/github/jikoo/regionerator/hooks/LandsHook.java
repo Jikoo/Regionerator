2cdb66bc6bca17e9e51465a9933fbc00db4f8ed9
Angeschossen <bytesoft.public@gmail.com>
2020-08-22 17:27:36 +0200

    Update LandsAPI (#24)
    * Remove deprecated method
    
    * Remove completeableFuture
    
    * Initialize LandsIntegration class
    
    * Initialize LandsIntegration class
    
    * LandsAPI is available via JitPack
    
    * Back to tabs
    
    Co-authored-by: Angeschossen <!FlashLight2018>
    Co-authored-by: Jikoo <jikoo.games@gmail.com>

--- src/main/java/com/github/jikoo/regionerator/hooks/LandsHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/LandsHook.java
@@ -1,10 +1,9 @@
 package com.github.jikoo.regionerator.hooks;
 
 import com.github.jikoo.regionerator.Regionerator;
-import java.util.concurrent.ExecutionException;
 import me.angeschossen.lands.api.integration.LandsIntegration;
-import me.angeschossen.lands.api.land.LandWorld;
 import org.bukkit.World;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * PluginHook for <a href=https://www.spigotmc.org/resources/lands.53313/>Lands</a>.
@@ -26,31 +25,21 @@ public class LandsHook extends PluginHook {
 
 	@Override
 	public boolean isChunkProtected(World chunkWorld, int chunkX, int chunkZ) {
-		LandWorld landWorld = getLandsAPI().getLandWorld(chunkWorld.getName());
-
-		if (landWorld == null) {
-			return false;
-		}
-
-		try {
-			return landsAPI.isClaimed(chunkWorld.getName(), chunkX, chunkZ).get();
-		} catch (InterruptedException | ExecutionException | ClassCastException e) {
-			e.printStackTrace();
-			return true;
-		}
-	}
-
-	private LandsIntegration getLandsAPI() {
-		if (landsAPI == null) {
-			landsAPI = new LandsIntegration(Regionerator.getPlugin(Regionerator.class), false);
-			landsAPI.initialize();
-		}
-		return landsAPI;
+		return getLandsAPI().isClaimed(chunkWorld, chunkX, chunkZ);
 	}
 
 	@Override
 	public boolean isAsyncCapable() {
 		return true;
+	}
+
+	@NotNull
+	private LandsIntegration getLandsAPI() {
+		if (landsAPI == null) {
+			landsAPI = new LandsIntegration(Regionerator.getPlugin(Regionerator.class));
+		}
+
+		return landsAPI;
 	}
 
 }
