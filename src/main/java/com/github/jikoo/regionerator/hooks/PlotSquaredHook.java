ac133ba591d41720e91409745eeebc8a708baead
Adam <Jikoo.Games@gmail.com>
2023-02-26 06:49:47 -0500

    Add support for PlotSquared (#157)
    As always, plugins that I do not have an environment set up for are not tested. Use at your own risk.

---
+++ src/main/java/com/github/jikoo/regionerator/hooks/PlotSquaredHook.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015-2023 by Jikoo.
+ *
+ * Regionerator is licensed under a Creative Commons
+ * Attribution-ShareAlike 4.0 International License.
+ *
+ * You should have received a copy of the license along with this
+ * work. If not, see <http://creativecommons.org/licenses/by-sa/4.0/>.
+ */
+
+package com.github.jikoo.regionerator.hooks;
+
+import com.github.jikoo.planarwrappers.util.Coords;
+import com.plotsquared.core.PlotSquared;
+import com.plotsquared.core.plot.PlotArea;
+import com.sk89q.worldedit.math.BlockVector3;
+import com.sk89q.worldedit.regions.CuboidRegion;
+import org.bukkit.World;
+
+import java.util.Arrays;
+
+public class PlotSquaredHook extends PluginHook {
+
+  public PlotSquaredHook() {
+    super("PlotSquared");
+  }
+
+  @Override
+  public boolean isChunkProtected(World chunkWorld, int chunkX, int chunkZ) {
+    int chunkBlockX = Coords.chunkToBlock(chunkX);
+    int chunkBlockZ = Coords.chunkToBlock(chunkZ);
+
+    BlockVector3 bottom = BlockVector3.at(chunkBlockX, 0, chunkBlockZ);
+    BlockVector3 top = BlockVector3.at(chunkBlockX + 15, 255, chunkBlockZ + 15);
+
+    CuboidRegion region = new CuboidRegion(bottom, top);
+
+    PlotArea[] plotAreas = PlotSquared.platform().plotAreaManager().getPlotAreas(chunkWorld.getName(), region);
+    return Arrays.stream(plotAreas).anyMatch(area -> area.getPlots().size() > 0);
+  }
+
+  @Override
+  public boolean isAsyncCapable() {
+    return true;
+  }
+
+}
