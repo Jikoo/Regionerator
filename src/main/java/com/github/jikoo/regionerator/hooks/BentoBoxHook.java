c320fa45f6dc786f81e02370e288b89af434605c
BONNe <bonne@bonne.id.lv>
2023-03-06 18:03:55 +0200

    Address review issues.

--- src/main/java/com/github/jikoo/regionerator/hooks/BentoBoxHook.java
+++ src/main/java/com/github/jikoo/regionerator/hooks/BentoBoxHook.java
@@ -1,5 +1,6 @@
 package com.github.jikoo.regionerator.hooks;
 
+import com.github.jikoo.planarwrappers.util.Coords;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
@@ -23,17 +24,39 @@ public class BentoBoxHook extends PluginHook
 	{
 		if (!BentoBox.getInstance().getIWM().inWorld(chunkWorld))
 		{
+			// This falls out as soon as world is not detected as BentoBox world.
 			return false;
 		}
 
-		int locX = chunkX << 4;
-		int locZ = chunkZ << 4;
+		int distanceBetweenIslands = BentoBox.getInstance().getIWM().getWorldSettings(chunkWorld).getIslandDistance();
+		
+		if (distanceBetweenIslands <= 0)
+		{
+			// Failed BentoBox config file. This is impossible to reach.
+			return false;
+		}
+		
+		// Distance between islands is always a half of actual value.
+		final int increment = Math.max(distanceBetweenIslands, 8) * 2 - 1;
+
+		int locX = Coords.chunkToBlock(chunkX);
+		int locZ = Coords.chunkToBlock(chunkZ);
 
 		IslandsManager manager = BentoBox.getInstance().getIslandsManager();
 
-		return manager.getIslandAt(new Location(chunkWorld, locX, 0, locZ)).isPresent() ||
-			manager.getIslandAt(new Location(chunkWorld, locX + 15, 0, locZ)).isPresent() ||
-			manager.getIslandAt(new Location(chunkWorld, locX + 15, 0, locZ + 15)).isPresent() ||
-			manager.getIslandAt(new Location(chunkWorld, locX, 0, locZ + 15)).isPresent();
+		for (int x = 0; x < 16; x = x + increment)
+		{
+			for (int z = 0; z < 16; z = z + increment)
+			{
+				if (manager.getIslandAt(new Location(chunkWorld, locX + x, 0, locZ + z)).isPresent())
+				{
+					// Protected island area
+					return true;
+				}
+			}
+		}
+
+		// Fallthrough.
+		return false;
 	}
 }
